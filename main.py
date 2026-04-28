cb1225a95f502d8a2eefcc7d68cb2d69cf3afb51
Rohit041118 <rohitkhond2004@gmail.com>
2026-04-28 20:33:14 +0530

    port change

--- main.py
+++ main.py
@@ -137,4 +137,4 @@ if __name__ == "__main__":
     print("  [AI] Fashion Recommendation Expert System")
     print("  Running at: http://127.0.0.1:5000")
     print("=" * 60)
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=80)
